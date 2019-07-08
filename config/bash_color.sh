f8432b9d8f2459d81b1b36d7be521f720bd452ca
maochuan <maochuan@bytedance.com>
2019-07-08 16:34:04 +0800

    use solarized light as default ls color

--- config/bash_color.sh
+++ config/bash_color.sh
@@ -49,7 +49,7 @@ esac
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    export LS_COLORS='no=00:fi=00:di=01;33:ln=01;36:pi=40;33:so=01;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:ex=01;35:*.cmd=01;35:*.exe=01;35:*.com=01;35:*.btm=01;35:*.bat=01;35:*.sh=01;35:*.csh=01;35:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.bz=01;31:*.tz=01;31:*.rpm=01;31:*.cpio=01;31:*.jpg=01;35:*.gif=01;35:*.bmp=01;35:*.xbm=01;35:*.xpm=01;35:*.png=01;35:*.tif=01;35:ow=01;36' 
+    # export LS_COLORS='no=00:fi=00:di=01;33:ln=01;36:pi=40;33:so=01;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:ex=01;35:*.cmd=01;35:*.exe=01;35:*.com=01;35:*.btm=01;35:*.bat=01;35:*.sh=01;35:*.csh=01;35:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.bz=01;31:*.tz=01;31:*.rpm=01;31:*.cpio=01;31:*.jpg=01;35:*.gif=01;35:*.bmp=01;35:*.xbm=01;35:*.xpm=01;35:*.png=01;35:*.tif=01;35:ow=01;36'
     alias ls='ls --color=auto'
     #alias dir='dir --color=auto'
     #alias vdir='vdir --color=auto'
--- config/install_bash_config.sh
+++ config/install_bash_config.sh
@@ -2,14 +2,25 @@
 
 mkdir -p ~/.bin/local/
 
+curl https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark --output ~/.dircolors
+
 echo 'export TERM=xterm-256color' >> ~/.bash_profile
 
 echo 'LOCAL_BIN_PATH=~/.bin/local/' >> ~/.bash_profile
 echo 'PYTHON_BIN_PATH=~/.local/bin' >> ~/.bash_profile
 echo 'export PATH=$LOCAL_BIN_PATH:$PYTHON_BIN_PATH:$PATH' >> ~/.bash_profile
 
-cp bash_color.sh ~/.bash_color
-cp bash_alias.sh ~/.bash_alias
+if [ -f "~/.bash_color" ]; then
+	echo "~/.bash_color exists, skip..."
+else
+	cp bash_color.sh ~/.bash_color
+fi
+
+if [ -f "~/.bash_alias" ]; then
+	echo "~/.bash_alias exists, skip..."
+else
+	cp bash_alias.sh ~/.bash_alias
+fi
 
 echo 'source ~/.bash_color' >> ~/.bash_profile
 echo 'source ~/.bash_alias' >> ~/.bash_profile
